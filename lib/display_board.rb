4981d3e19009ddb1a9fc9558cb823a4ec634c23f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-08-29 03:10:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,21 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+    sLine = ""
+    count = 0
+    board = Array.new(9, " ")
+
+    3.times{
+  	puts tab_line(count, board)
+  	if count < 2
+  		puts "-----------"
+  	end
+    }
+end
+
+
+def tab_line
+    line = "   |   |   "
+	return line
+end
+
+display_board
